18a8b4dac76ebf6f4c83dfa62f1831683ee6f98a
Elliott.zheng <elliott.zheng@foxmail.com>
2017-05-29 13:33:37 +0800

    change icon source tinify png

--- app/src/main/java/com/elliott/a18350/irecognizer/ClearEditText.java
+++ app/src/main/java/com/elliott/a18350/irecognizer/ClearEditText.java
@@ -36,7 +36,7 @@ public class ClearEditText extends android.support.v7.widget.AppCompatEditText i
         // 获取EditText的DrawableRight,假如没有设置我们就使用默认的图片,获取图片的顺序是左上右下（0,1,2,3,）
         mClearDrawable = getCompoundDrawables()[2];
         if (mClearDrawable == null) {
-            mClearDrawable = getResources().getDrawable(R.drawable.delete);
+            mClearDrawable = getResources().getDrawable(R.drawable.cross);
         }
 
         mClearDrawable.setBounds(0, 0, mClearDrawable.getIntrinsicWidth(),
--- app/src/main/java/com/elliott/a18350/irecognizer/InfoActivity.java
+++ app/src/main/java/com/elliott/a18350/irecognizer/InfoActivity.java
@@ -3,7 +3,10 @@ package com.elliott.a18350.irecognizer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.KeyEvent;
+import android.widget.TextView;
 
 /**
  * Created by 18350 on 2017/5/25 0025.
@@ -14,6 +17,9 @@ public class InfoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+        TextView textView = (TextView) findViewById(R.id.hyperlink);
+        textView.setAutoLinkMask(Linkify.ALL);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
